57e950b3156dc3269d91166786dde732c03c971e
Mingtaros <leonardow41@gmail.com>
2020-09-20 12:19:10 +0700

    Added CatDog Data

--- src/conv2d.py
+++ src/conv2d.py
@@ -3,14 +3,20 @@ from activation import *
 from util import conv2d
 
 class Conv2D:
-  def __init__(self, num_filter, pad, stride, input_shape=(1, 1, 3), activation='relu'):
-    self.kernel = np.random.random((num_filter, input_shape[2], w_filter, h_filter)) # w_filter and h_filter TBD @adityaputras
+  def __init__(self, num_filter, kernel_shape, pad, stride, input_shape=(1, 1, 3), activation='relu',):
+    if (len(kernel_shape) != 2):
+      raise ValueError("Kernel shape must be in 2 Dimension")
+    self.kernel = np.random.random((num_filter, input_shape[2], kernel_shape[0], kernel_shape[1]))
     self.pad = pad
     self.stride = stride
+    self.input_shape = input_shape
+    self.output_shape = None
 
     if (activation == 'relu'):
       self.activation = relu # detector part
       self.activation_deriv = relu_deriv
+    else:
+      raise ValueError("Activation function " + activation + " does not exist.")
 
   def updateInputShape(self, input_shape):
     self.input_shape = input_shape
@@ -41,10 +47,10 @@ class Conv2D:
     return np.array(result)
 
   def calcPrevDelta(self, neuron_input, delta, debug=False):
-		return delta
+    return delta
 
   def backprop(self, neuron_input, delta, lr=0.001, debug=False):
     pass
 
   def updateWeight(self, deltaWeight, debug=False):
-		pass
+    pass
--- src/util.py
+++ src/util.py
@@ -10,7 +10,7 @@ def conv2d(mat, kernel, pad, stride):
   num_filter, kernel_c, kernel_x, kernel_y = kernel.shape
   output_shape = ((padded_mat_x - kernel_x) // stride + 1, (padded_mat_y - kernel_y) // stride + 1, num_filter)
   output = np.zeros(output_shape)
-  for filter in range(num_filter):
+  for _filter in range(num_filter):
     for i in range(output_shape[0]):
       start_x = i*stride
       end_x = start_x + kernel_x
@@ -18,7 +18,7 @@ def conv2d(mat, kernel, pad, stride):
         start_y = j*stride
         end_y = start_y + kernel_y 
         for chan in range(padded_mat_c):
-          output[i, j, filter] += np.tensordot(padded_mat[start_x:end_x, start_y:end_y, chan], kernel[filter, min(chan, kernel_c-1), :, :]) 
+          output[i, j, _filter] += np.tensordot(padded_mat[start_x:end_x, start_y:end_y, chan], kernel[_filter, min(chan, kernel_c-1), :, :]) 
   return output
 
 # Pooling function for 2d matrices
