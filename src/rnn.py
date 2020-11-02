73bb3076a5c3bc650a3ae2e7b29094a87da425df
VinsenMarselino <vinsen@xendit.co>
2020-11-02 16:56:15 +0700

    feat: add forward rnn

--- src/rnn.py
+++ src/rnn.py
@@ -2,13 +2,15 @@ import numpy as np
 from activation import *
 
 class RNN(object):
-  def __init__(self, units, hidden_size, input_shape=None, activation='tanh', return_sequences=False):
-    self.units = units # output dim
-    self.hidden_size = hidden_size
+  def __init__(self, units, initialize_weight='random', input_shape=None, activation='tanh', return_sequences=False):
+    self.units = units
+    self.initialize_weight = initialize_weight
     self.input_shape = input_shape
     self.return_sequences = return_sequences
     self.activation_name = activation
     self.output_shape = None
+    self.h = []
+    self.h.append(self.initWeight((self.units, )))
     
     if (self.activation_name == 'relu'):
       self.activation = relu
@@ -25,27 +27,32 @@ class RNN(object):
     else:
       raise ValueError("Activation function " + activation + " does not exist.")
 
-    self.bias_xh = np.random.random((self.hidden_size,)) # bias for input to hidden
-    self.bias_hy = np.random.random((self.units,)) # bias for hidden to output
+    self.bias_xh = self.initWeight((self.units,)) # bias for input to hidden
     # 3 weights
     self.U = None
-    self.V = None
     self.W = None
 
     if (self.input_shape != None):
       self.updateWBO()
   
+  def initWeight(self, size):
+    if self.initialize_weight == 'random':
+      return np.random.random(size)
+    elif self.initialize_weight == 'zeros':
+      return np.zeros(size)
+    elif self.initialize_weight == 'ones':
+      return np.ones(size)
+
   def updateInputShape(self, input_shape):
     self.input_shape = input_shape
     self.updateWBO()
 
   def updateWBO(self):
-    sequence_length = self.input_shape[0]
+    sequence_length = self.input_shape[1]
 
     # Initialize 3 weight matrices (U, V, W)
-    self.U = np.random.random((self.hidden_size, sequence_length))
-    self.V = np.random.random((self.units, self.hidden_size))
-    self.W = np.random.random((self.hidden_size, self.hidden_size))
+    self.U = self.initWeight((self.units, sequence_length))
+    self.W = self.initWeight((self.units, self.units))
 
     if (self.return_sequences == True):
       # output = returning full output sequence
@@ -63,24 +70,34 @@ class RNN(object):
       "activation": self.activation_name,
       "data": {
         "bias_xh": self.bias_xh.tolist(),
-        "bias_yh": self.bias_hy.tolist(),
         "U": self.U.tolist(),
-        "V": self.V.tolist(),
         "W": self.W.tolist()
       }
     }
 
   def loadData(self, data):
     self.bias_xh = np.array(data['bias_xh'].copy())
-    self.bias_hy = np.array(data['bias_hy'].copy())
 
     self.U = np.array(data['U'].copy())
-    self.V = np.array(data['V'].copy())
     self.W = np.array(data['W'].copy())
 
   def forward(self, x_data):
-    # TBD
-    return x_data
+    # self.U = np.array([[0.1, 0.15, 0.2, 0.3],
+    #                   [0.15, 0.2, 0.3, 0.1],
+    #                   [0.2, 0.3, 0.1, 0.15]])
+    # self.W = np.array([[0.5, 0.5, 0.5],
+    #                   [0.5, 0.5, 0.5],
+    #                   [0.5, 0.5, 0.5]])
+    # self.bias_xh = np.array([0.1, 0.1, 0.1])
+    for timestep in range(self.input_shape[0]):
+      ux = self.U @ x_data[timestep]
+      wh = self.W @ self.h[timestep]
+      ht = np.tanh(ux + (wh + self.bias_xh))
+      self.h.append(ht)
+    if (self.return_sequences == True):
+      return self.h
+    else:
+      return self.h[-1]
 
   # Backward Propagation Methods
   def calcPrevDelta(self, neuron_input, delta, debug=False):
